ae6d2afe2f53aab8b743ce0c04d9112fb8c0d46b
Ariel ADEROMOU <149815504+femiariel@users.noreply.github.com>
2025-08-01 17:25:48 +0200

    Update Ari.py

--- Ari.py
+++ Ari.py
@@ -2,27 +2,9 @@
 
 F2::
 {
-    ; Vide le presse-papiers
-    origClipboard := A_Clipboard
-    A_Clipboard := ""
     Send "^c"
-    
     if !ClipWait(1)
-    {
-        MsgBox "Erreur : Aucun texte sélectionné ou la copie a échoué."
         return
-    }
 
-    inputText := A_Clipboard
-
-    ; === Traitement global ===
-    ; Remplace deux sauts de ligne ou plus par un seul
-    ; Compatible avec \n ou \r\n (Unix ou Windows)
-    cleaned := RegExReplace(inputText, "(\r?\n){2,}", "`r`n")
-
-    ; Met le résultat dans le presse-papiers
-    A_Clipboard := cleaned
-
-    ; Message rapide
-    TrayTip "AutoHotkey", "Sauts de ligne multiples remplacés.", 2000
+    A_Clipboard := RegExReplace(A_Clipboard, "(\r?\n){2,}", "`r`n")
 }
